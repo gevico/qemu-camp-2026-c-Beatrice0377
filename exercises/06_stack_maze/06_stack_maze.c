5be974421fac4c58932ddc1fc7909670d364bd93
cnb.cMFPtpS7QFA <tGBLfrNeHSflZhHIaa6UjO+cnb.cMFPtpS7QFA@noreply.cnb.cool>
2026-04-16 15:31:01 +0800

    no.6

--- exercises/06_stack_maze/06_stack_maze.c
+++ exercises/06_stack_maze/06_stack_maze.c
@@ -11,10 +11,62 @@ int maze[MAX_ROW][MAX_COL] = {
 	0, 0, 0, 1, 0,
 };
 
+typedef struct{
+	int row;
+	int col;
+}Point;
+
+Point stack[MAX_COL*MAX_ROW];
+
+int top=-1;
+
+void push(int r,int c){
+	top++;
+	stack[top].row=r;
+	stack[top].col=c;
+}
+
+void pop(){
+	if(top>=0)top--;
+}
+
 int main(void)
 {
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
-
+	int start_r=0,start_c=0;
+	int end_r=4,end_c=4;
+	push(start_r,start_c);
+	maze[start_r][start_c]=2;
+	while(top>=0){
+		int r=stack[top].row;
+		int c=stack[top].col;
+		if(r==end_r&&c==end_c){
+			for(int i=0;i<=top;i++){
+				printf("(%d,%d)\n",stack[i].row,stack[i].col);
+			}
+			return 0;
+		}
+		else if(r<MAX_ROW&&c<MAX_COL&&maze[r][c]==0){
+			maze[r][c]=2;
+			push(r,c);
+		}
+		else if(r<MAX_ROW&&c+1<MAX_COL&&maze[r][c+1]==0){
+			maze[r][c+1]=2;
+			push(r,c+1);
+		}
+		else if(r+1<MAX_ROW&&c<MAX_COL&&maze[r+1][c]==0){
+			maze[r+1][c]=2;
+			push(r+1,c);
+		}
+		else if(r-1>=0&&c<MAX_COL&&maze[r-1][c]==0){
+			maze[r-1][c]=2;
+			push(r-1,c);
+		}
+		else if(r<MAX_ROW&&c-1>=0&&maze[r][c-1]==0){
+			maze[r][c-1]=2;
+			push(r,c-1);
+		}
+		else pop();
+	}
+	printf("No path!\n");
 	return 0;
 }
