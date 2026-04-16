9d70f5e11b6c000c89476a3a9c02e4eba460e16b
cnb.cMFPtpS7QFA <tGBLfrNeHSflZhHIaa6UjO+cnb.cMFPtpS7QFA@noreply.cnb.cool>
2026-04-16 18:41:10 +0800

    no.8

--- exercises/08_circular_queue/08_circular_queue.c
+++ exercises/08_circular_queue/08_circular_queue.c
@@ -18,9 +18,28 @@ int main() {
     Queue q;
     int total_people=50;
     int report_interval=5;
-
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+	for(int i=0;i<total_people;i++){
+		q.data[i].id=i+1;
+	}
+    q.head=0;
+	q.tail=49;
+	q.count=50;
+    int report_num=1;
+    while(q.count>1){
+        if(report_num==report_interval){
+            printf("淘汰: %d\n", q.data[q.head].id);
+			q.head=(q.head+1)%MAX_PEOPLE;
+			q.count--;
+			report_num=1;
+        }
+        else{
+            People p=q.data[q.head];
+            q.head=(q.head+1)%MAX_PEOPLE;
+            q.tail=(q.tail+1)%MAX_PEOPLE;
+            q.data[q.tail]=p;
+			report_num++;
+        }
+    }
     
     printf("最后剩下的人是: %d\n", q.data[q.head].id);
 
